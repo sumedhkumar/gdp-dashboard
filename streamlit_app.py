06681c27661c7de0932cfe2eca873c15a49d3b99
Sumedhkumar <sumedhkumar.bhalerao@builder.ai>
2024-10-11 23:39:09 +0530

    removed unnecessory imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
-import streamlit_authenticator as stauth
-import hashlib
-
 
 # Sample users (replace with your authentication logic)
 users = {
